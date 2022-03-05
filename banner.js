f3ebd1720b7b2e9d5be124e189378346cf28d031
Sephiroth <78663409+sephirothcoding@users.noreply.github.com>
2022-03-05 14:23:57 +0200

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,34 +1,7 @@
-$(document).ready(function()
-{
-    $('.contact').click(function (e) 
-    {
-        $('.card').toggleClass('active');
-        $('.banner').toggleClass('active');
-        $('.photo').toggleClass('active');
-        $('.social-media-banner').toggleClass('active');
-        $('.email-form').toggleClass('active');  
-        var buttonText = $('button.contact#main-button').text();
-        if (buttonText === 'back')
-        {
-            buttonText = 'click to get in touch';
-            $('button.contact#main-button').text(buttonText);
-        }
-        else
-        {
-            buttonText = 'back';
-            $('button.contact#main-button').text(buttonText);
-        }
-    });
+$('.ribbon').on('click', function() {	
+  var self = $(this),
+      newone = self.clone(true);
+  
+  self.before(newone);
+  $("." + self.attr("class") + ":last").remove();
 });
-//turn on to make the photo follow mouse
-// $(document).ready(function()
-// {
-//     $(document).mousemove(function( event ) 
-//     {
-//         var docWidth = $(document).width();
-//         var docHeight = $(document).height();
-//         var xValue = (event.clientX/docWidth)*100;
-//         var yValue = (event.clientY/docHeight)*100;
-//         $('.photo').css('background-position', xValue+'%,'+yValue+'%');
-//     });
-// });
